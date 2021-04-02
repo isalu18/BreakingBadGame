509fc415e25dc6db4110bb7ae6913d2d88bed6cd
isalu18 <luke_shakz@hotmail.com>
2021-04-02 13:30:08 -0700

    Project finished

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Frase from './components/Frase';
 import styled from '@emotion/styled';
 
@@ -17,6 +17,12 @@ const Boton = styled.button`
   padding: 1rem 3rem;
   font-size: 2rem;
   border: 2px solid black;
+  transition: background-size 0.5s ease; 
+
+  :hover {
+    cursor: pointer;
+    background-size: 400px;
+  }
 `;
 
 function App() {
@@ -30,6 +36,11 @@ function App() {
     const frase = await api.json();
     guardarFrase(frase[0]);
   }
+
+  //Cargar una frase
+  useEffect(() => {
+    consultarAPI();
+  }, []);
 
   return (
     <Contenedor>
